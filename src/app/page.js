09c1c9e3331dbfe5fa090b9c5beefd0e161da6ad
unknown <mohmmadalean1@gmaile.com>
2024-08-22 00:08:20 +0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,26 @@
 import Image from "next/image";
 import bg from "../../public/background/home-background.png";
+import RenderModel from "@/components/RenderModel";
+import Wizard from "@/components/models/Wizard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
       <Image
         className="w-full h-full object-cover object-center opacity-25"
+        fill
         src={bg}
         alt="background"
       />
 
-      <div className="w-full h-screen">{/* navigation and 3d model */}</div>
+      <div className="w-full h-screen">
+        {
+          /* navigation and 3d model */
+          <RenderModel>
+            <Wizard />
+          </RenderModel>
+        }
+      </div>
     </main>
   );
 }
